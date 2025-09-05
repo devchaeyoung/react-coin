c2d94f25f45be4c0f11b57fced39820b5931d083
devchaeyoung <chaemaa00@gmail.com>
2025-09-06 02:57:16 +0900

    fix: 오타수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const base = process.env.NODE_ENV === 'production' ? '/reacr-coin/' : '';
+const base = process.env.NODE_ENV === 'production' ? '/react-coin/' : '';
 
 // https://vite.dev/config/
 export default defineConfig({
